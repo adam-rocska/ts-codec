165a989bbac5b41d29a9b2594f69682f7b46957a
Adam Laszlo Rocska <adam-rocska@users.noreply.github.com>
2023-03-28 00:00:52 +0200

    Nothing is an easy one.

---
+++ test/unit/json/nothing.test.ts
@@ -0,0 +1,12 @@
+import {nothing} from '#json/nothing';
+
+describe('JSON Codec : nothing', () => {
+  test('#encode', async () => {
+    expect(nothing.encode()).toStrictEqual('');
+  });
+
+  test('An Encode-Decode cycle should bring us to where we were.', () => {
+    expect(nothing.decode(nothing.encode())).toStrictEqual(undefined);
+  });
+
+});
