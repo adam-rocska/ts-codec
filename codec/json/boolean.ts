662d58a0cbd5a65969d4fef902ff6cb1f6a95ef6
Adam Laszlo Rocska <adam-rocska@users.noreply.github.com>
2023-03-28 00:07:41 +0200

    Was it easy? Yes.

--- codec/json/boolean.ts
+++ codec/json/boolean.ts
@@ -1,6 +1,6 @@
 import {Codec} from '#Codec';
 
 export const boolean: Codec<boolean> = {
-  decode: v => v.length > 0,
-  encode: v => (v ? '1' : ''),
+  decode: v => v === 'true',
+  encode: v => (v ? 'true' : 'false'),
 };
---
+++ test/unit/json/boolean.ts
@@ -0,0 +1,13 @@
+import {boolean} from '#json/boolean';
+
+describe('JSON Codec : nothing', () => {
+  test('#encode', async () => {
+    expect(boolean.encode(true)).toStrictEqual('true');
+    expect(boolean.encode(false)).toStrictEqual('false');
+  });
+
+  test('An Encode-Decode cycle should bring us to where we were.', () => {
+    expect(boolean.decode(boolean.encode(true))).toStrictEqual(true);
+    expect(boolean.decode(boolean.encode(false))).toStrictEqual(false);
+  });
+});
