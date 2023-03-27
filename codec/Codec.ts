44851f577fcf51c580371b92878b247db2920e95
Adam Laszlo Rocska <adam-rocska@users.noreply.github.com>
2023-03-27 18:40:27 +0200

    Types are ok. Let's migrate code now... already have much better ideas.

--- codec/Codec.ts
+++ codec/Codec.ts
@@ -1,6 +1,37 @@
+/**
+ * The default datatype used as the raw datasource type.
+ */
 export type RawData = string;
-export type Encoder<D, C = RawData> = (d: D) => C;
-export type Decoder<D, C = RawData> = (c: C) => D;
+
+/**
+ * Convenient type representing a function mapping `T` to `F`.
+ * @typeParam F - The type from which mapping is performed.
+ * @typeParam T – The type to which mapping is performed.
+ */
+export type Coder<F, T> = (f: F) => T;
+
+/**
+ * Represents a function responsible for encoding the provided data.
+ * @typeParam D - The encoded datatype.
+ * @typeParam C - The target to which to encode `D`. By default it's `RawData`.
+ */
+export type Encoder<D, C = RawData> = Coder<D, C>;
+
+/**
+ * Represents a function responsible for decoding the provided raw data.
+ * @typeParam D - The type to which this decoder decodes `C`.
+ * @typeParam C - The raw data type to decode as `D`.
+ */
+export type Decoder<D, C = RawData> = Coder<C, D>;
+
+/**
+ * Represents an object capable of performing encoding & decoding based type
+ * conversion between `C` and `D`.
+ * @typeParam D - Represents the type which is encoded or to which `C` is
+ *                decoded to.
+ * @typeParam C - Represents the type to which `D` is encoded to or which
+ *                `D` is decoded from.
+ */
 export type Codec<D, C = RawData> = {
   encode: Encoder<D, C>;
   decode: Decoder<D, C>;
