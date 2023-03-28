fa7da88a45a134485f940714bd56fb4930b459d7
Adam Laszlo Rocska <adam-rocska@users.noreply.github.com>
2023-03-28 15:12:52 +0200

    This test might seem dumb, but it'll be handy on refactors.

---
+++ test/unit/Exception.test.ts
@@ -0,0 +1,18 @@
+import {Codec} from '#Codec';
+import {Exception} from '#Exception';
+
+describe('Exception', () => {
+  describe('.constructor', () => {
+    test('should provide referential access to the related codec', () => {
+      const stubCodec: Codec<undefined> = {
+        encode: () => '',
+        decode: () => undefined,
+      };
+      const testMessage = 'Test Message.';
+      const testException = new Exception(stubCodec, testMessage);
+      expect(testException.cause).toBeUndefined();
+      expect(testException.message).toEqual(testMessage);
+      expect(testException.codec).toBe(stubCodec);
+    });
+  });
+});
