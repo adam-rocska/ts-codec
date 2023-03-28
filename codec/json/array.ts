f6bc4fd96cdf1fc0bbafa7f00e14dbf594efd174
Adam Laszlo Rocska <adam-rocska@users.noreply.github.com>
2023-03-28 22:54:40 +0200

    We don't have a full API doc coverage yet. But we gotta ship.

--- codec/json/array.ts
+++ codec/json/array.ts
@@ -1,13 +1,25 @@
 import {Codec} from '#Codec';
 
-export const array = <V>(valueCodec: Codec<V>): Codec<Array<V>> => ({
+/**
+ * @summary Constructs a {@link Codec} for an array of `V`.
+ * @param valueCodec The {@link Codec} encoding values of `V`.
+ * @typeParameter V The type of values to be contained in the array.
+ * @returns The built {@link Codec} of `Array<V>`.
+ * @see {@link "json" | The JSON Codec Module}
+ * @example
+ * ```ts
+ * import {json} from "@21gram-consulting/ts-codec";
+ *
+ * const numbers = json.array(json.number);
+ * ```
+ */ export const array = <V>(valueCodec: Codec<V>): Codec<Array<V>> => ({
   encode: a => JSON.stringify(a.map(v => valueCodec.encode(v))),
   decode: s =>
     JSON.parse(s, (key, value) =>
       key === ''
         ? value
         : Number.isNaN(parseInt(key, 10))
-        ? console.warn('Received a non-list value.', key, value)
-        : valueCodec.decode(value)
+          ? console.warn('Received a non-list value.', key, value)
+          : valueCodec.decode(value)
     ),
 });
